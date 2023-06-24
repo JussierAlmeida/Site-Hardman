92a2f426af0031c163ee7942e9e7f89fa85720c4
JussierAlmeida <127342987+JussierAlmeida@users.noreply.github.com>
2023-06-24 20:22:13 -0300

    cria função abre e fecha menu. esta com erro. abre mas não fecha.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 //seleção de elementos  
-//const menu_btn = document.querySelector("#menu");
-//const close_menu_btn = document.querySelector("close-menu");
-
-
-
+const menu_btn = document.querySelector("#menu");
+const close_menu_btn = document.querySelector("close-menu");
+const menu = document.querySelector("#mobile-navbar");
 //funçoes
 
 
 
 //eventos
+[menu_btn,close_menu_btn].forEach((btn) =>{
+    btn.addEventListener("click",(e)=>{
+        menu.classList.toggle("menu-active");
+    })
+})
