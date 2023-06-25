8c40664284abc8fc94c6ae74147e0812d5325dfa
JussierAlmeida <127342987+JussierAlmeida@users.noreply.github.com>
2023-06-24 22:22:22 -0300

    finaliza no banner aereo. as outras funções estão nas outras branch.

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,25 @@
 //const close_menu_btn = document.querySelector("close-menu");
 
 
-
+const slides = document.querySelectorAll(".banner");
+const dots = document.querySelectorAll(".dot");
+let slideIndex = 0;
 //funçoes
+function showSlides(){
+    console.log(slides);
 
+    for (let i =0; i<slides.length; i++){
+        slides[i].classList.remove("active");
+        dots[i].classList.remove("active");
+    }
+    slideIndex++
+    if(slideIndex > slides.length){
+        slideIndex = 1;
+    }
+    slides[slideIndex- 1].classList.add("active");
+    dots[slideIndex- 1].classList.add("active");
+    setTimeout(showSlides, 3000);
+}
 
 
 //eventos
