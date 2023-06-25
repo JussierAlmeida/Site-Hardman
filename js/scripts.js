f7cf6505ed907744a82fdef446257cf2ae6102d4
JussierAlmeida <127342987+JussierAlmeida@users.noreply.github.com>
2023-06-24 21:21:07 -0300

    ancoragem dos links. feita ancoragem e criada a functio smooth no site

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,25 @@
 //const menu_btn = document.querySelector("#menu");
 //const close_menu_btn = document.querySelector("close-menu");
 
+const desktoplinks = document.querySelectorAll("#navbar a");
+const mobilelinks = document.querySelectorAll("#mobile-navbar a");
+const alllinks = [...desktoplinks,...mobilelinks];
 
+console.log(alllinks);
 
 //funçoes
-
+function smoothScroll(e){ 
+   // e.preventDefault();
+   const href = this.getAttribute("href");
+   const offsetTop = document.querySelector(href).offsetTop;
+    scroll({
+        top: offsetTop,
+        behavior: "smooth",
+    });
+}
 
 
 //eventos
+alllinks.forEach((link)=>{
+    link.addEventListener("click",smoothScroll);
+});
