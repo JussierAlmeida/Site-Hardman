eb82dea3e2aed65ad21ac2a30121feb5d7817c7a
JussierAlmeida <127342987+JussierAlmeida@users.noreply.github.com>
2023-06-27 22:41:48 -0300

    foi corigido os erros. erros dos banners e do fechar menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,69 @@
 //seleção de elementos 
-//cria js
+//cria js----------------------------------ok
 const menu_btn = document.querySelector("#menu");
 const close_menu_btn = document.querySelector("#close-menu");
 const menu = document.querySelector("#mobile-navbar");
 
-//cria menu aereo
+//cria menu aereo--------------------------ok
 const desktoplinks = document.querySelectorAll("#navbar a");
 const mobilelinks = document.querySelectorAll("#mobile-navbar a");
 const alllinks = [...desktoplinks,...mobilelinks];
 
-console.log(alllinks);
-//banner aereo
+//banner aereo---------------------------------ok
 const slides = document.querySelectorAll(".banner");
 const dots = document.querySelectorAll(".dot");
 let slideIndex = 0;
 
 //funçoes
-//banner aereo
+
+//---------------------------------ok
+function smoothScroll(e){ 
+   e.preventDefault();
+
+   const href = this.getAttribute("href");
+   const offsetTop = document.querySelector(href).offsetTop;
+
+    scroll({
+        top: offsetTop,
+        behavior: "smooth",
+    });
+
+    setTimeout(()=>{
+        if(menu.classList.contains("menu-active")){
+        menu.classList.remove("menu-active");
+        }
+    },500);
+}
+//banner aereo--------------------------------ok
 function showSlides(){
-    console.log(slides);
+
     for (let i =0; i<slides.length; i++){
         slides[i].classList.remove("active");
         dots[i].classList.remove("active");
     }
-    slideIndex++
+    slideIndex++;
 
     if(slideIndex > slides.length){
         slideIndex = 1;
     }
 
-    slides[slideIndex- 1].classList.add("active");
-    dots[slideIndex- 1].classList.add("active");
+    slides[slideIndex - 1].classList.add("active");
+    dots[slideIndex - 1].classList.add("active");
 
     setTimeout(showSlides, 3000);
 }
-function smoothScroll(e){ 
-   e.preventDefault();
-   const href = this.getAttribute("href");
-   const offsetTop = document.querySelector(href).offsetTop;
-    scroll({
-        top: offsetTop,
-        behavior: "smooth",
-    });
-    setTimeout(()=>{
-        if(menu.classList.contains(menu-active)){
-        menu.classList.remove("menu-active");
-        }
-    },500);
-}
+
 //eventos
-//cria js
+//cria js----------------------------ok
 [menu_btn,close_menu_btn].forEach((btn) =>{
     btn.addEventListener("click",(e)=>{
         menu.classList.toggle("menu-active");
-    })
-})
+    });
+});
 
-//cria menu aereo
+//cria menu aereo ------------------ok
 alllinks.forEach((link)=>{
     link.addEventListener("click",smoothScroll);
 });
+//inicialização---------------------ok
+showSlides();
